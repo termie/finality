0b091ba47f4d50f72b1d91c2f99c4304901b8b1b
termie <github@anarkystic.com>
2012-07-05 11:14:30 -0700

    done?

--- final/lasting/views.py
+++ final/lasting/views.py
@@ -4,20 +4,30 @@ from django import template
 from django import shortcuts
 from django.template import loader
 
+from google.appengine.api import memcache
 from google.appengine.ext import db
 
 from final import models
 
 DECLINED = 'This offender declined to make a last statement.'
 
+def _get_data():
+  data = memcache.get("shown_offenders")
+  if data is not None:
+    return data
+  else:
+    offenders = db.GqlQuery('SELECT * '
+                            'FROM Offender '
+                            'WHERE show = True '
+                            'ORDER BY number ')
+    offenders = list(offenders)
+    memcache.add("shown_offenders", offenders)
+    return offenders
+
 def index(request):
   #t = loader.get_template('templates/index.html')
   #c = template.RequestContext(request, locals())
-  offenders = db.GqlQuery('SELECT * '
-                          'FROM Offender '
-                          'WHERE show = True '
-                          'ORDER BY number ')
-  offenders = list(offenders)
+  offenders = _get_data()
   random.shuffle(offenders)
   # Don't show photos for those without statements.
   #offenders = [x for x in offenders
